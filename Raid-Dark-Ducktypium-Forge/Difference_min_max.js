545e715d71e1306242cc726aff6c07d4101722db
Michelle Mannering <36594527+MishManners@users.noreply.github.com>
2022-07-12 13:29:20 +1000

    add let

--- Raid-Dark-Ducktypium-Forge/Difference_min_max.js
+++ Raid-Dark-Ducktypium-Forge/Difference_min_max.js
@@ -1,8 +1,8 @@
 function differenceMinMax(array) {
 
-  array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  differenceMinMax = Math.max(array) - Math.min(array);
+  let differenceMinMax = Math.max(array) - Math.min(array);
 
   console.log(differenceMinMax);
 
