2efd47915d536fef616e6d4dec1cd3dccbe8936c
Michelle Mannering <36594527+MishManners@users.noreply.github.com>
2022-07-12 14:44:24 +1000

    Punctuation works
    Still one missing.

--- Raid-Dark-Ducktypium-Forge/reverse_sentence.js
+++ Raid-Dark-Ducktypium-Forge/reverse_sentence.js
@@ -1,5 +1,8 @@
 function reverseSentence(sentence) {
 
+    // one line reverse with no other changes
+    // reverseSentence = sentence.split(" ").reverse().join(" ");
+
     // lowercase the first letter of the sentence
     sentence = sentence.charAt(0).toLowerCase() + sentence.slice(1);
 
@@ -8,23 +11,10 @@ function reverseSentence(sentence) {
 
     // split the punctuation
     const punctuationArray = sentence.match(/[.,;:!?]/g);
-    delete punctuationArray[punctuationArray.length - 1];
 
     // reverse the sentence and join it together
     const reversedSentence = sentenceArray.reverse().join(" ");
     
-    // uppercase the first letter of the sentence
-    return reversedSentence.charAt(0).toUpperCase() + reversedSentence.slice(1) + punctuationArray;
+    // uppercase the first letter of the sentence and add punctuation again
+    return reversedSentence.charAt(0).toUpperCase() + reversedSentence.slice(1) + punctuationArray;    
 }
-
-
-/*
-    Reverse words with no changes
-    const lowerCaseWord = sentence.toLowerCase();
-    const words = sentence.split(" ");
-    const reversedWords = [];
-    for (var i = words.length - 1; i >= 0; i--) {
-        reversedWords.push(words[i]);
-    }
-    const reversedSentence = sentence.toUpperCase();
-    return reversedWords.join(" "); */
