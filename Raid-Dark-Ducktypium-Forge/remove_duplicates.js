1a8d61c316355cf1844e1cd9f963eb010fb722b0
Michelle Mannering <36594527+MishManners@users.noreply.github.com>
2022-07-12 15:09:57 +1000

    Solved duplicates
    Test solved!

--- Raid-Dark-Ducktypium-Forge/remove_duplicates.js
+++ Raid-Dark-Ducktypium-Forge/remove_duplicates.js
@@ -2,8 +2,26 @@ function removeDuplicateCharacters(string) {
 
  // remove ALL duplicates: return string.split('').filter((item, index, inputArray) => inputArray.indexOf(item) === index).join('');
 
-    return string.split('').filter((char, index, arr) => arr.indexOf(char) === index).join('');
+    function removeDuplicateCharacters(string) {
+        const nonDuplicateCharacters = [];
+        let previousCharacter
+
+        for (let i = 0; i < string.length; i++) {
+            const currentCharacter = string[i];
+
+            if (currentCharacter !== previousCharacter) {
+                nonDuplicateCharacters.push(currentCharacter);
+            }
+
+            previousCharacter = currentCharacter;
+        }
+        return nonDuplicateCharacters.join('');
+    }
+
+
 }
+
+
 
 console.log("Test case 1:");
 console.log(removeDuplicateCharacters("aabbcc"));
