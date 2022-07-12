87c7462c83b87b7fe3151ba7e2d9e0386da07bc3
Michelle Mannering <36594527+MishManners@users.noreply.github.com>
2022-07-12 13:36:48 +1000

    Sum function

--- Raid-Dark-Ducktypium-Forge/Difference_min_max.js
+++ Raid-Dark-Ducktypium-Forge/Difference_min_max.js
@@ -1,6 +1,6 @@
 function differenceMinMax(array) {
 
-  let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   let differenceMinMax = Math.max(array) - Math.min(array);
 
---
+++ Raid-Dark-Ducktypium-Forge/Sum_array.js
@@ -0,0 +1,19 @@
+function sumArray(numbers) {
+    // TODO: Sum the numbers!
+    
+      var sum = 0;
+      for (var i = 0; i < numbers.length; i++) {
+          sum += numbers[i];
+      }
+      return sum;
+  }
+    
+  
+  console.log("Test case 1:");
+  console.log(sumArray([1, 2, 3]));
+  
+  console.log("Test case 2:");
+  console.log(sumArray([-1, 0, 1]));
+  
+  console.log("Test case 3:");
+  console.log(sumArray([100, 12.0, -4.5]));
